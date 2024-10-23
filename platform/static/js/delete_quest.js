ec6f65dabc96f395a68dee668287f8bc0b90b88b
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-10-23 22:35:34 +0300

    [platform] using fetch api in question deleting

--- platform/static/js/delete_quest.js
+++ platform/static/js/delete_quest.js
@@ -7,22 +7,51 @@ $(document).ready(function() {
         let $form = $(this)
         let questionId = $form.find("input[name=questionId]").val()
 
-        $.ajax({
-            url: '/',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
+        fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
                 questionId,
                 action: 'deleteQuest'
-            }),
-            success: function(response) {
-                if (response.deleteQuestion) {
-                    $form.closest('.question').remove()
-                }
-            },
-            error: function(response) {
-                console.log(response.responseJSON.error)
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw response
+            }
+            return response.json()
+        })
+        .then(data => {
+            if (data.deleteQuestion) {
+                $form.closest('.question').remove()
             }
         })
+        .catch(error => {
+            if (error instanceof Response) {
+                error.json().then(err => {
+                    console.error(err.error)
+                })
+            }
+        })
+
+        // $.ajax({
+        //     url: '/',
+        //     type: 'POST',
+        //     contentType: 'application/json',
+        //     data: JSON.stringify({
+        //         questionId,
+        //         action: 'deleteQuest'
+        //     }),
+        //     success: function(response) {
+        //         if (response.deleteQuestion) {
+        //             $form.closest('.question').remove()
+        //         }
+        //     },
+        //     error: function(response) {
+        //         console.log(response.responseJSON.error)
+        //     }
+        // })
     })
 })
