7b9cad706cf7d4453dfff1552d49a5c6bcdebe8d
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-10-29 10:35:40 +0200

    [platform] return to jquery ajax

--- platform/static/js/delete_quest.js
+++ platform/static/js/delete_quest.js
@@ -8,41 +8,23 @@ $(document).ready(function() {
         let $form = $(this)
         let questionId = $form.find("input[name=questionId]").val()
 
-        fetch('/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
+        $.ajax({
+            url: '/',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
                 questionId,
                 action: 'deleteQuest'
-            })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw response
-            }
-            return response.json()
-        })
-        // Successfully deleted question
-        .then(data => {
-            if (data.deleteQuestion) {
-                $form.closest('.question').remove()
-            }
-        })
-
-        // Possible errors
-        .catch(error => {
-            // Check if error is an instance of Response class
-            // It is checked to ensure that the request has been sent, even if it is not correct
-            if (error instanceof Response) {
-                error.json().then(err => {
-                    console.error(err.error)
-                })
+            }),
+            success: function(response) {
+                if (response.deleteQuestion) {
+                    // Deleting .question div where the delete button
+                    $form.closest('.question').remove()
+                }
+            },
+            error: function(response) {
+                console.log(response.responseJSON.error)
             }
         })
     })
 })
-
-// Response.json() takes the Response stream and reads it to the end
-// It returns a promise that resolves to the result of parsing the response body as a string
