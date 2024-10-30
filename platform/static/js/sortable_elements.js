db4cf53f06dae5c786ce7ab43ac9ae0dd5e1cb85
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-10-30 20:01:59 +0200

    [platform] cleaner code

--- platform/static/js/sortable_elements.js
+++ platform/static/js/sortable_elements.js
@@ -1,35 +1,40 @@
 $(document).ready(function () {
     const $questions = $("#created-questions")
 
-    // Check if there are elements that can be sorted
-    if ($questions.length) {
-        new Sortable($questions[0], {
-            animation: 150,
-            onEnd: function () {
-                const order = []
-                const containerCells = $questions.children()
-                
-                // Prepare order before being sent to the server
-                containerCells.each(function (index) {
-                    const cellId = $(this).data("question-id")
-                    order.push({
-                        id: cellId,
-                        order: index + 1
+    function sortingElements($container, dataAttr) {
+        // Check if there are elements that can be sorted
+        if ($container.length) {
+            new Sortable($container[0], {
+                animation: 150,
+                onEnd: function () {
+                    const order = []
+                    const containerCells = $container.children()
+                    
+                    // Prepare order before being sent to the server
+                    containerCells.each(function (index) {
+                        const cellId = $(this).data(dataAttr)
+                        order.push({
+                            id: cellId,
+                            order: index + 1
+                        })
                     })
-                })
-
-                $.ajax({
-                    url: "/",
-                    type: "POST",
-                    contentType: 'application/json',
-                    data: JSON.stringify({
-                        cell_order: order,
-                        action: "cell_order_upgrade",
-                    }),
-                    success: function () {}
-                })
-            }
-        })
-    }
     
+                    $.ajax({
+                        url: "/",
+                        type: "POST",
+                        contentType: 'application/json',
+                        data: JSON.stringify({
+                            cell_order: order,
+                            action: "cell_order_upgrade",
+                        })
+                    })
+                }
+            })
+        }
+    }
+
+    sortingElements(
+        $container=$questions,
+        dataAttr="question-id"
+    )
 })
