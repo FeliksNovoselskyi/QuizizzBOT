d417ec83f96bd7ff348e7dc7cda4ac81a0a07b60
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-12-21 01:45:13 +0200

    [platform] fix switch theme button text during switching a theme

--- platform/static/js/darkThemeSwitch.js
+++ platform/static/js/darkThemeSwitch.js
@@ -4,5 +4,10 @@ const switchThemeButton = document.querySelector('.switch-theme')
 
 switchThemeButton.addEventListener('click', () => {
     body.classList.toggle("dark-theme")
-    // switchThemeButton.textContent = "Light"
+
+    if (body.classList.contains("dark-theme")) {
+        switchThemeButton.textContent = "Light"
+    } else {
+        switchThemeButton.textContent = "Dark"
+    }
 })
