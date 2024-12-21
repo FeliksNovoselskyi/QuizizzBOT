bc0ec82c2a71b36d4ec8deb4d9a5358798bc31b1
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-12-21 02:04:53 +0200

    [platform] add theme saving in localstorage

--- platform/static/js/darkThemeSwitch.js
+++ platform/static/js/darkThemeSwitch.js
@@ -2,12 +2,22 @@
 const body = document.body
 const switchThemeButton = document.querySelector('.switch-theme')
 
+if (localStorage.getItem("theme") == "dark") {
+    body.classList.add("dark-theme")
+    switchThemeButton.textContent = "Light"
+} else {
+    body.classList.remove("dark-theme")
+    switchThemeButton.textContent = "Dark"
+}
+
 switchThemeButton.addEventListener('click', () => {
     body.classList.toggle("dark-theme")
 
     if (body.classList.contains("dark-theme")) {
         switchThemeButton.textContent = "Light"
+        localStorage.setItem("theme", "dark")
     } else {
+        localStorage.setItem("theme", "light")
         switchThemeButton.textContent = "Dark"
     }
 })
